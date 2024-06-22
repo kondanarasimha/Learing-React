47819f943be7cea33288e2c3dd78f4d154b408cd
Narasimha <kondanarasimha123@gmail.com>
2024-06-22 14:20:29 +0530

    About JSX and Bable Transpiler

--- README.md
+++ README.md
@@ -15,3 +15,13 @@
 - Good Error handling
 - HTPPS
 - different dev and production(prod) bundles
+
+# JSX
+- JSX is a javascript XML, JSX is not a html or xml, it sepeart syntax
+- JSX is not understand by browers.
+
+# BABLE
+- Bable is transpiler or Compailer, it converts the jsx into React element.
+- The React Element is object that object will convert into html with the help of ReactDOM(render Method).
+- This is Example how it work - 'const heading = <h1>hello world</h1>' BABLE CONVERT INTO 'const heading = React.createElement('h1',{},'hello world')'
+- Bable package coming from parcel.
--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//This is the way of creating nesting div's.
-const heading = React.createElement('div',{id:'parent'},[ 
-  React.createElement('div',{id:'child1'},[
-    React.createElement('h1',{id:'h1'},'This is h1'),
-    React.createElement('h2',{id:'h2'},'This is h2')
-  ]),
-  React.createElement('div',{id:'child2'},[
-    React.createElement('h1',{id:'h1'},'This is h1'),
-    React.createElement('h2',{id:'h2'},'This is h2')
-  ])]
-);
+//This JSX code will convert into react with the help of bable this is package coming with parcel.
+//Bable is a transpiler which convert into React element. React element is a object by using render method object into html. 
+const heading = <h1 id="heading">This is a JSX</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(heading);
